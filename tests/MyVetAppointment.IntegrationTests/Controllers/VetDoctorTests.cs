46414f38cf2b47042c48d979244d11967c66b29b
Gologan Nicoleta <nicoletagologan85@yahoo.com>
2022-11-28 12:34:25 +0200

    get_vet_test

---
+++ tests/MyVetAppointment.IntegrationTests/Controllers/VetDoctorTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Net;
+using MyVetAppointment.IntegrationTests.Config;
+using NUnit.Framework;
+
+namespace MyVetAppointment.IntegrationTests.Services;
+
+[TestFixture]
+public class VetDoctorTests : CustomBaseTest
+{
+
+    [Test]
+    public async Task GET_VetDoctors_ShouldBe_Status_OK()
+    {
+        //Arrange
+        var client = GetClient();
+
+        //Act
+        var response = await client.GetAsync("/VetDoctor/vets");
+
+        //Assert
+        response.StatusCode.Equals(HttpStatusCode.OK);
+     }
+}
